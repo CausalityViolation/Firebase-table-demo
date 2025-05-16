b3e39c571f1e992ef3bb9525b3ed1f6d59a22cdf
Causality <martin.uddner@returbo.io>
2025-05-16 15:33:26 +0200

    remove comments, add search function and fix height of table

--- ui/src/lib/table/table.component.ts
+++ ui/src/lib/table/table.component.ts
@@ -1,13 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { List } from '@org/shared';
 
 @Component({
   selector: 'lib-table',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.css'],
 })
 export class TableComponent {
   @Input() data: List[] = [];
+
+  filteredGames: List[] = [];
+  searchTerm = '';
+
+  onSearchChange() {
+    const term = this.searchTerm.toLowerCase().trim();
+    this.filteredGames = this.data.filter((game) =>
+      game.title.toLowerCase().includes(term)
+    );
+  }
 }
